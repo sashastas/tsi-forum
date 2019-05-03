7a95dd0526df6cd253c167cfb09d1a2121d7d894
Aleksandrs Stasjuļevičs <st60916@TTI.LV>
2019-05-03 10:28:32 +0300

    Tests

--- src/test/java/lv/tsi/forum/resource/ForumResourceTest.java
+++ src/test/java/lv/tsi/forum/resource/ForumResourceTest.java
@@ -2,10 +2,27 @@ package lv.tsi.forum.resource;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 
 class ForumResourceTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void getAllTopics() {
+    void getAllTopics() throws Exception{
+        mockMvc.perform(get("/forum/getAll")).andExpect(status().isOk());
     }
 }
